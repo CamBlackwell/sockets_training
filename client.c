9804b9c51a3983cdb8204760bdf68f91b9d8ed4d
CamBlackwell <cameronblackwell89@gmail.com>
2025-11-18 11:04:40 +1030

    feat: created client

---
+++ client.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8888;
+#define BUFFER_SIZE 1024;
+
+int main(){
+    int client_fd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in server_addr;
+    int bytes_read;
+
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);//creates a socket for server 
+    if (client_fd < 0){
+        perror("socket error");
+        exit(1);
+    }
+
+
+    memset(&server_addr, 0, sizeof(server_addr)); //sets up a server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) < 0){ //turns to binary //SWITCH THIS AFTER IT WORKS TO MULTIPLE DEVICES
+        perror("invalid address");
+        exit(1);
+    }
+
+    if (connect(client_addr, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        perror("failed to connect");
+        exit(1);
+    }
+
+    printf("connected to server. type messages CTRL D to quit :)\n");
+
+    while(fgets(buffer, BUFFER_SIZE, stdin) != NULL ){ //loops and puts stdin into buffer
+        if (send(client_fd, buffer, BUFFER_SIZE) < 0)[
+            perror("send error");
+            break;
+        ]
+
+        memset(buffer, 0, BUFFER_SIZE);
+        bytes_read = recv(sock_fd, buffer, BUFFER_SIZE-1, 0);
+        if (bytes_read < 0){
+            printf("server disconnected");
+            break;
+        }
+
+        printf("server echoes: %s", buffer);
+
+    }
+    close(client_addr);
+    return 0;
+}
